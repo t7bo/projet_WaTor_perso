8fc2992f9f649dfa2f0f6eee3f3b519f377f3042
t7bo <tibo7w@gmail.com>
2023-11-01 00:31:08 +0100

    fichier fonctionnel

--- monde.py
+++ monde.py
@@ -3,3 +3,9 @@ class Monde:
     def __init__(self, largeur, hauteur):
         self.largeur = largeur
         self.hauteur = hauteur
+
+# Création d'une instance de Monde
+monde = Monde(20, 10)  # Largeur et hauteur du monde
+
+# Initialisation de la grille
+grille = [["💧" for _ in range(monde.largeur)] for _ in range(monde.hauteur)]
